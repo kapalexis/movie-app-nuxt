06abf0de7a79f196c1a738c2a37782f256c55ee9
kapalexis <kap.alexis@gmail.com>
2022-04-22 11:55:09 +0200

    undo restyle from branch

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,6 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/vue-youtube-player.js', ssr: false },
-    { src: '~/plugins/vue-plyr.js', mode: 'client' },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
